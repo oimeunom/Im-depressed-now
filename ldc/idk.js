c755414da1c5037a1f2d06b8999696a4d0ac7f3a
oimeunom <109981363+oimeunom@users.noreply.github.com>
2022-10-26 14:43:18 -0300

    Add files via upload

---
+++ ldc/idk.js
@@ -0,0 +1,46 @@
+var NomeDasPessoas = [];
+    
+function enviar()
+{
+    var NomeConvidado = document.getElementById("nome1").value;
+ NomeDasPessoas.push(NomeConvidado);
+
+	console.log(NomeConvidado);
+        
+    console.log(NomeDasPessoas);
+    var ComprimentoDoNome = NomeDasPessoas.length; 
+    console.log(ComprimentoDoNome);
+	document.getElementById("NomeEmExibiçao3").innerHTML=NomeDasPessoas.toString();
+	
+   }
+
+function organizar()
+{
+	NomeDasPessoas.sort(); 
+	var i= NomeDasPessoas.join("<br>");
+    console.log(NomeDasPessoas);		
+	document.getElementById("sorted").innerHTML=i.toString();
+	}
+
+function mostrar()
+{
+    var i= NomeDasPessoas.join("<br>");
+	console.log(NomeDasPessoas);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sortButton").style.display="block";
+	}
+
+function pesquisar()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<NomeDasPessoas.length; j++)
+		{
+			if(s==NomeDasPessoas[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="Nome encontrado "+found+" vez(es)";
+	console.log("Nome encontrado "+found+" vez(es)");
+}
